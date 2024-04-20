0d7c44f9614758e23309f2183cbc4dff1d4ce295
Trevor Cook <cook.trevor@outlook.com>
2024-04-20 15:15:32 -0600

    test update from macbook

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 // This effectively makes the Slint code a macro and allows you to combine the UI and business logic
 // At some point I'll probably move this to its own .slint file but this is fine for now
 slint::slint!{
-    export component AppWindow {
-        Text {
-            text: "radio check";
-            color: blue;
+    import { LineEdit } from "std-widgets.slint";
+export component AppWindow inherits Window{
+        LineEdit {
+            font-size: 14px;
+            width: 200px;
+            height: 25px;
+            input-type: number;
+            placeholder-text: "Enter income here.";
         }
     }  
 }
