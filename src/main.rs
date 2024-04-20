3135bee704ff8c45757d0aa503cdafc0a934ad7d
Trevor Cook <cook.trevor@outlook.com>
2024-04-20 14:21:55 -0600

    initial slint setup and comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+// This effectively makes the Slint code a macro and allows you to combine the UI and business logic
+// At some point I'll probably move this to its own .slint file but this is fine for now
+slint::slint!{
+    export component AppWindow {
+        Text {
+            text: "radio check";
+            color: blue;
+        }
+    }  
+}
 
-
+// Main app loop
 fn main() {
-    println!("Hello, world!");
+    AppWindow::new().unwrap().run().unwrap(); // That's not ugly or anything
 }
