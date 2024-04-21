32d44128135f1fbf44262c2475034d870c4664f7
Trevor Cook <cook.trevor@outlook.com>
2024-04-20 21:35:28 -0600

    tinkering with the ui

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
 // This effectively makes the Slint code a macro and allows you to combine the UI and business logic
 // At some point I'll probably move this to its own .slint file but this is fine for now
 slint::slint!{
-    import { LineEdit } from "std-widgets.slint";
+    import { LineEdit, Slider } from "std-widgets.slint";
 export component AppWindow inherits Window{
+        width: 320px;
+        height: 240px;
+    VerticalLayout {
+        spacing: 5px;
+
+        // User data entry point
         LineEdit {
+            horizontal-alignment: center;
             font-size: 14px;
             width: 200px;
             height: 25px;
             input-type: number;
             placeholder-text: "Enter income here.";
         }
-    }  
+    
+        // 
+        Text {
+            text: "Housing";
+        }
+
+        Slider {
+            width: parent.width * 80%;
+            height: parent.height;
+            value: 25;
+            minimum: 0;
+            maximum: 100;
+        }
+    }
+}  
 }
 
 // Main app loop
 fn main() {
-    AppWindow::new().unwrap().run().unwrap(); // That's not ugly or anything
+    AppWindow::new().unwrap().run().unwrap(); // That's not hideous at all...
 }
